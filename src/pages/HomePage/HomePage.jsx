f73d54f242d89d5236da8859ba599c2cf6626fb7
Carl Joseph <carljoseph156@gmail.com>
2025-03-09 20:08:00 -0400

    Added/Updated Homepage page

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomePage.css";
+const HomePage = () => {
+  return (
+    <>
+      <h2>Welcome to Pastria!</h2>
+      <p> It's a pleasure to host you and yours!.</p>
+    </>
+  );
+};
+
+export default HomePage;
