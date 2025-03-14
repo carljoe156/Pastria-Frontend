eca8cda478609c1039a6d7bf14ff61d0c271bb90
Carl Joseph <carljoseph156@gmail.com>
2025-03-14 16:39:16 -0400

    Added/Updated Welcome component

---
+++ src/components/Welcome/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div>
+      <h2>Welcome to Pastria!</h2>
+      <p>It's a pleasure to host you and yours!</p>
+    </div>
+  );
+};
+
+export default Welcome;
